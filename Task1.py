44a565b06d0abf956dd9e5bd5e76ac29d18f3181
maxgavaga <ferrarif138@yandex.ru>
2022-09-29 13:24:53 +0300

    Homework

---
+++ Task1.py
@@ -0,0 +1,17 @@
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+def Del_Word(s):
+    return False if 'абв' in s else True
+    #for i in range(len(s)-2):
+    #    if (s[i]+s[i+1]+s[i+2]) == 'абв':
+    #        f =  False
+    #return f
+
+
+print('Введите текст ')
+a = input()
+
+a = a.split()
+print(a)
+a = list(filter(Del_Word,a))
+print(a)
